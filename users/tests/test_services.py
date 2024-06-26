9576d385f951de02fd09f4f7d7a182d29ff71d87
Marko Yaminsky <marko.yaminskiy03@gmail.com>
2024-06-26 15:23:40 +0300

    Add CRUD operations for users

--- users/tests/test_services.py
+++ users/tests/test_services.py
@@ -2,8 +2,18 @@ import pytest
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 
-from users.services import create_user, delete_user, get_all_users, update_user
-from users.tests.factories import UserFactory
+from users.models import Team
+from users.services import (
+    create_team,
+    create_user,
+    delete_team,
+    delete_user,
+    get_all_teams,
+    get_all_users,
+    update_team,
+    update_user,
+)
+from users.tests.factories import TeamFactory, UserFactory
 
 User = get_user_model()
 
@@ -70,7 +80,53 @@ class TestDeleteUserService:
         user = UserFactory()
         not_deleted_user = UserFactory()
 
-        delete_user(user=user)
+        delete_user(user)
 
         assert User.objects.count() == 1
         assert User.objects.first() == not_deleted_user
+
+
+class TestCreateTeamService:
+    name = "Survey Corps"
+
+    def test_successful(self):
+        team = create_team(name=self.name)
+
+        assert Team.objects.count() == 1
+        assert Team.objects.first() == team
+
+
+class TestGetAllTeamsService:
+    name = "Survey Corps"
+
+    def test_success(self):
+        team = TeamFactory(name=self.name)
+        TeamFactory()
+
+        teams = get_all_teams(name=self.name)
+
+        assert teams.count() == 1
+        assert teams.first() == team
+
+
+class TestUpdateTeamService:
+    new_name = "Survey Corps"
+
+    def test_successful(self):
+        team = UserFactory(name="Not Survey Corps")
+
+        update_team(team=team, name=self.new_name)
+
+        team.refresh_from_db()
+        assert team.name == self.new_name
+
+
+class TestDeleteTeamService:
+    def test_successful(self):
+        team = TeamFactory()
+        not_deleted_team = TeamFactory()
+
+        delete_team(team)
+
+        assert Team.objects.count() == 1
+        assert Team.objects.first() == not_deleted_team
